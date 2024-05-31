d80b56d01a4b252abd5f9b8ac94f44f44f0b3f81
nisslyak <nissopodolyak@gmail.com>
2024-05-31 06:22:50 +0300

    Adding the files

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0, j = 0; i < arr.Count; i++, j++)
+        {
+            sum1 = sum1 + arr[i][j];
+        }
+
+        for (int k = 0, l = arr.Count - 1; l >= 0; k++, l--)
+        {
+            sum2 = sum2 + arr[k][l];
+        }
+        return Math.Abs(sum2 - sum1);
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = Result.diagonalDifference(arr);
+        }
+    }
+}
